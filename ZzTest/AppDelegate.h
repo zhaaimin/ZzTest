a5614a0e7fc8c727f8e2a53097d63aec5989575e
lion <lion@liondeMac-mini.local>
2014-07-08 11:28:05 +0800

    Initial Commit

---
+++ ZzTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZzTest
+//
+//  Created by lion on 14-7-8.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
